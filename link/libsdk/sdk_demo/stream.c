84fa71af9e3572e9eb7a2baa591f089b89dc8257
liyq <liyuanquan@qiniu.com>
2018-07-05 11:53:10 +0800

    code structure change

--- link/libsdk/sdk_demo/stream.c
+++ link/libsdk/sdk_demo/stream.c
@@ -1,4 +1,4 @@
-// Last Update:2018-07-04 10:11:43
+// Last Update:2018-07-05 10:15:23
 /**
  * @file stream.c
  * @brief push rtp strem  
@@ -6,7 +6,6 @@
  * @version 0.1.00
  * @date 2018-06-28
  */
-#include "devsdk.h"
 #include "main.h"
 #include "sdk_interface.h"
 #include "common.h"
@@ -46,33 +45,4 @@ int AudioGetFrameCb( char *_pFrame, int _nLen, double _dTimeStamp,
     return 0;
 }
 
-int InitIPC()
-{
-    int context = 0;
-    int s32Ret = 0;
-    AudioConfig audioConfig;
 
-    s32Ret = dev_sdk_init( DEV_SDK_PROCESS_APP );
-    if ( s32Ret < 0 ) {
-        DBG_ERROR("dev_sdk_init error, s32Ret = %d\n", s32Ret );
-        return -1;
-    }
-    dev_sdk_start_video( 0, 0, VideoGetFrameCb, &context );
-    dev_sdk_get_AudioConfig( &audioConfig );
-    if ( audioConfig.audioEncode.enable ) {
-        dev_sdk_start_audio_play( AUDIO_TYPE_G711 );
-        dev_sdk_start_audio( 0, 1, AudioGetFrameCb, NULL );
-    } else {
-        DBG_ERROR("not enabled\n");
-    }
-
-    return 0;
-}
-
-int DeInitIPC()
-{
-    dev_sdk_stop_video(0, 1);
-    dev_sdk_stop_audio(0, 1);
-    dev_sdk_stop_audio_play();
-    dev_sdk_release();
-}
