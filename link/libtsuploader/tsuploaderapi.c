cd584ba52fa5940424d23539df37a562a4e7f2ad
lmk <963547817@qq.com>
2018-07-20 14:15:17 +0800

    tsuploader: add missing file

---
+++ link/libtsuploader/tsuploaderapi.c
@@ -0,0 +1,167 @@
+#include "tsmuxuploader.h"
+#include <assert.h>
+#include "log.h"
+#include "base.h"
+
+static char gAk[65] = {0};
+static char gSk[65] = {0};
+static char gBucket[65] = {0};
+static char gUid[129] = {0};
+static char gDeviceId[129] = {0};
+static char gToken[256] = {0};
+static int nIsInited = 0;
+static TsMuxUploader *gpTsMuxUploader = NULL;
+
+int UpdateToken(char * pToken)
+{
+        int ret = 0;
+        ret = snprintf(gToken, sizeof(gToken), "%s", pToken);
+        assert(ret > 0);
+        if (ret == sizeof(gUid)) {
+                logerror("token:%s is too long", pToken);
+                return TK_ARG_ERROR;
+        }
+        
+        return 0;
+}
+
+int SetBucketName(char *_pName)
+{
+        int ret = 0;
+        ret = snprintf(gBucket, sizeof(gBucket), "%s", _pName);
+        assert(ret > 0);
+        if (ret == sizeof(gBucket)) {
+                logerror("bucketname:%s is too long", _pName);
+                return TK_ARG_ERROR;
+        }
+        
+        return 0;
+}
+
+int InitUploader(char * _pUid, char *_pDeviceId, char *_pBucketName, char * _pToken)
+{
+        if (nIsInited) {
+                return 0;
+        }
+        
+        Qiniu_Global_Init(-1);
+
+        int ret = 0;
+        ret = UpdateToken(_pToken);
+        if (ret != 0) {
+                return ret;
+        }
+        ret = SetBucketName(_pBucketName);
+        if (ret != 0) {
+                return ret;
+        }
+
+        ret = snprintf(gUid, sizeof(gUid), "%s", _pUid);
+        assert(ret > 0);
+        if (ret == sizeof(gUid)) {
+                logerror("uid:%s is too long", _pUid);
+                return TK_ARG_ERROR;
+        }
+        
+        ret = snprintf(gDeviceId, sizeof(gDeviceId), "%s", _pDeviceId);
+        assert(ret > 0);
+        if (ret == sizeof(gDeviceId)) {
+                logerror("deviceid:%s is too long", _pDeviceId);
+                return TK_ARG_ERROR;
+        }
+        
+        ret = StartMgr();
+        if (ret != 0) {
+                logerror("StartMgr fail\n");
+                return 0;
+        }
+        logdebug("main thread id:%ld\n", (long)pthread_self());
+        logdebug("main thread id:%ld\n", (long)pthread_self());
+
+        ret = NewTsMuxUploader(&gpTsMuxUploader);
+        if (ret != 0) {
+                logerror("NewTsMuxUploader fail\n");
+                return ret;
+        }
+
+        gpTsMuxUploader->SetToken(gpTsMuxUploader, gToken);
+        ret = TsMuxUploaderStart(gpTsMuxUploader);
+        if (ret != 0){
+                logerror("UploadStart fail:%d\n", ret);
+                return ret;
+        }
+
+        return 0;
+}
+
+int PushVideo(char * _pData, int _nDataLen, int64_t _nTimestamp, int _nIsKeyFrame, int _nIsSegStart)
+{
+        assert(gpTsMuxUploader != NULL);
+        int ret = 0;
+        ret = gpTsMuxUploader->PushVideo(gpTsMuxUploader, _pData, _nDataLen, _nTimestamp, _nIsKeyFrame);//, _nIsSegStart);
+        return ret;
+}
+
+int PushAudio(char * _pData, int _nDataLen, int64_t _nTimestamp)
+{
+        assert(gpTsMuxUploader != NULL);
+        int ret = 0;
+        ret = gpTsMuxUploader->PushAudio(gpTsMuxUploader, _pData, _nDataLen, _nTimestamp);
+        return ret;
+}
+
+void UninitUploader()
+{
+        if (!nIsInited)
+                return;
+        DestroyTsMuxUploader(&gpTsMuxUploader);
+        StopMgr();
+        Qiniu_Global_Cleanup();
+}
+
+int SetAk(char *_pAk)
+{
+        int ret = 0;
+        ret = snprintf(gAk, sizeof(gAk), "%s", _pAk);
+        assert(ret > 0);
+        if (ret == sizeof(gAk)) {
+                logerror("sk:%s is too long", _pAk);
+                return TK_ARG_ERROR;
+        }
+        
+        return 0;
+}
+
+int SetSk(char *_pSk)
+{
+        int ret = 0;
+        ret = snprintf(gSk, sizeof(gSk), "%s", _pSk);
+        assert(ret > 0);
+        if (ret == sizeof(gSk)) {
+                logerror("sk:%s is too long", _pSk);
+                return TK_ARG_ERROR;
+        }
+        
+        return 0;
+}
+
+int GetUploadToken(char *pBuf, int nBufLen)
+{
+        if (gAk[0] == 0 || gSk[0] == 0 || gBucket[0] == 0)
+                return -11;
+        Qiniu_Mac mac;
+        mac.accessKey = gAk;
+        mac.secretKey = gSk;
+        
+        Qiniu_RS_PutPolicy putPolicy;
+        Qiniu_Zero(putPolicy);
+        putPolicy.scope = gBucket;
+        putPolicy.deleteAfterDays = 7;
+        
+        char *uptoken;
+        uptoken = Qiniu_RS_PutPolicy_Token(&putPolicy, &mac);
+        assert(nBufLen > strlen(uptoken));
+        strcpy(pBuf, uptoken);
+        Qiniu_Free(uptoken);
+        return 0;
+}
---
+++ link/libtsuploader/tsuploaderapi.h
@@ -0,0 +1,21 @@
+#ifndef __TS_UPLOADER_API__
+#define __TS_UPLOADER_API__
+
+#include "tsmuxuploader.h"
+#include "log.h"
+
+int InitUploader(char * pUid, char *pDeviceId, char *pBucketName, char * pToken);
+int UpdateToken(char * pToken);
+int PushVideo(char * pData, int nDataLen, int64_t nTimestamp, int nIsKeyFrame, int nIsSegStart);
+int PushAudio(char * pData, int nDataLen, int64_t nTimestamp);
+void UninitUploader();
+
+
+//for test
+int GetUploadToken(char *pBuf, int nBufLen);
+int SetAk(char *pAk);
+int SetSk(char *pSk);
+int SetBucketName(char *_pName);
+
+
+#endif
