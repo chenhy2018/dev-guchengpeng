b360bb4265158b873fd497c9e03eda038c0369ca
lizhengqian <li1zheng2qian3@gmail.com>
2018-07-30 17:39:13 +0800

    update env file

--- link/vod/env.sh
+++ link/vod/env.sh
@@ -1,4 +1,4 @@
 export QBOXROOT="$(dirname "$(dirname "$(pwd)")")/third_party"
-export GOPATH=$QBOXROOT/vendor/:`pwd`
+export GOPATH=$QBOXROOT/vendor/:`pwd`:$GOPATH
 source "$QBOXROOT/base/env.sh"
 source "$QBOXROOT/base/mockacc/env.sh"
