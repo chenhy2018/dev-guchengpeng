63aea2ee3929f9b499df8ebd96c28a3b48a2b5aa
lizhengqian <li1zheng2qian3@gmail.com>
2018-07-30 15:23:08 +0800

    move vendor to thiryparty && verify on docker link 1.0

---
+++ link/vod/env.sh
@@ -0,0 +1,4 @@
+export QBOXROOT=/opt2/linking/third_party
+source $QBOXROOT/base/env.sh
+source $QBOXROOT/base/env-mock.sh
+export GOPATH=$GOPATH:/opt2/linking/third_party/vendor/:/opt2/linking/link/vod
