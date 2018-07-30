45de00bc75360fc2574631a241a618bbeb4177a0
lizhengqian <li1zheng2qian3@gmail.com>
2018-07-30 17:01:24 +0800

    update env file && delete pkg dir

--- link/vod/env.sh
+++ link/vod/env.sh
@@ -1,4 +1,4 @@
 export QBOXROOT="$(dirname "$(dirname "$(pwd)")")/third_party"
-export GOPATH=$GOPATH:$QBOXROOT/vendor/:`pwd`
+export GOPATH=$QBOXROOT/vendor/:`pwd`
 source "$QBOXROOT/base/env.sh"
 source "$QBOXROOT/base/mockacc/env.sh"
