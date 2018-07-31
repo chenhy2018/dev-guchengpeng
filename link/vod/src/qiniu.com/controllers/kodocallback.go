7e0e43d7580337438437f4ae99f2989b09e27bdd
lizhengqian <li1zheng2qian3@gmail.com>
2018-07-31 12:05:29 +0800

    udpate kodocallback handler

--- link/vod/src/qiniu.com/controllers/kodocallback.go
+++ link/vod/src/qiniu.com/controllers/kodocallback.go
@@ -1,19 +1,26 @@
 package controllers
 
 import (
+	"encoding/json"
 	"io/ioutil"
-	"net/url"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/qiniu/xlog.v1"
 )
 
+type kodoCallBack struct {
+	Key    string `json:"key"`
+	Hash   string `json:"hash"`
+	Fsize  int64  `json:"fsize"`
+	Bucket string `json:"bucket"`
+	Name   string `json:"name"`
+}
+
 // sample requst see: https://developer.qiniu.com/kodo/manual/1653/callback
 func UploadTs(c *gin.Context) {
 	authHeader := c.Request.Header.Get("Authorization")
 
-	c.Header("Content-Type", "text/plain")
+	c.Header("Content-Type", "application/json")
 	xl := xlog.New(c.Writer, c.Request)
 
 	if !verifyAuth(xl, authHeader) {
@@ -22,6 +29,7 @@ func UploadTs(c *gin.Context) {
 		})
 		return
 	}
+
 	body, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
 		c.JSON(500, gin.H{
@@ -29,27 +37,18 @@ func UploadTs(c *gin.Context) {
 		})
 		return
 	}
-	values, err := url.ParseQuery(string(body))
-	if err != nil || len(values["name"]) == 0 {
-		c.JSON(500, gin.H{
-			"error": "read callback body falied",
-		})
-		return
-	}
+	xl.Infof("%s", body)
+	var kodoData kodoCallBack
+	err = json.Unmarshal(body, &kodoData)
+	xl.Infof("%s", kodoData)
+	fileName := kodoData.Name
 
-	fileName := strings.Split(values["name"][0], "_")
-	if len(fileName) < 3 {
-		c.JSON(500, gin.H{
-			"error": "bad file name",
-		})
-		return
-	}
-	xl.Infof("upload file = %s", values["name"][0])
+	xl.Infof("upload file = %s", fileName)
 	// filename = uid_devciceid_segmentid.ts
 	// models.addDevice(xl, fileName[0], fileName[1], fileName[2], time.Now().Unix())
 	c.JSON(200, gin.H{
 		"success": true,
-		"name":    values["name"][0],
+		"name":    fileName,
 	})
 }
 
--- link/vod/src/qiniu.com/main/main.go
+++ link/vod/src/qiniu.com/main/main.go
@@ -18,26 +18,26 @@ func main() {
 		fmt.Println("read conf file error, error = ", err)
 		os.Exit(3)
 	}
-        initDb()
+	initDb()
 	r.GET("/playback/:uid/:deviceId", controllers.PlayBackGetm3u8)
-	r.POST("/upload", controllers.UploadTs)
+	r.POST("/qiniu/upload/callback", controllers.UploadTs)
 	r.Run(Config.Bind) // listen and serve on 0.0.0.0:8080
 
 }
 
 func initDb() {
-        url := "mongodb://root:public@180.97.147.164:27017,180.97.147.179:27017/admin"
-        dbName := "vod"
-        config := db.MgoConfig {
-                Host : url,
-                DB   : dbName,
-                Mode : "",
-                Username : "root",
-                Password : "public",
-                AuthDB : "admin",
-                Proxies : nil,
-        }
-        if err := db.InitDb(&config); err != nil {
+	url := "mongodb://root:public@180.97.147.164:27017,180.97.147.179:27017/admin"
+	dbName := "vod"
+	config := db.MgoConfig{
+		Host:     url,
+		DB:       dbName,
+		Mode:     "",
+		Username: "root",
+		Password: "public",
+		AuthDB:   "admin",
+		Proxies:  nil,
+	}
+	if err := db.InitDb(&config); err != nil {
 		fmt.Println(err)
 		os.Exit(3)
 	}
