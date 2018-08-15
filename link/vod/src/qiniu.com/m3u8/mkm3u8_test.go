1a6859840e2214dc741a3390ef0503fb602fe680
liyq <liyuanquan@qiniu.com>
2018-08-15 10:52:38 +0800

    gofmt mkm3u8_test.go

--- link/vod/src/qiniu.com/m3u8/mkm3u8_test.go
+++ link/vod/src/qiniu.com/m3u8/mkm3u8_test.go
@@ -18,27 +18,27 @@ func TestMakem3u8_old() {
 }
 
 func TestMakem3u8() {
-    var playlist []map[string]interface{}
+	var playlist []map[string]interface{}
 
-    m1 := map[string]interface{}{
-        "duration": 10.00,
-        "url":      "http://pcgtsa42m.bkt.clouddn.com/7/testuid5/testdeviceid5/1533795689087/1533795686553.ts",
-    }
+	m1 := map[string]interface{}{
+		"duration": 10.00,
+		"url":      "http://pcgtsa42m.bkt.clouddn.com/7/testuid5/testdeviceid5/1533795689087/1533795686553.ts",
+	}
 
-    m2 := map[string]interface{}{
-        "duration": 9.88,
-        "url":      "http://pcgtsa42m.bkt.clouddn.com/7/testuid5/testdeviceid5/1533795689087/1533795686552.ts",
-    }
+	m2 := map[string]interface{}{
+		"duration": 9.88,
+		"url":      "http://pcgtsa42m.bkt.clouddn.com/7/testuid5/testdeviceid5/1533795689087/1533795686552.ts",
+	}
 
-    m3 := map[string]interface{}{
-        "duration": 9.88,
-        "url":      "http://pcgtsa42m.bkt.clouddn.com/7/testuid5/testdeviceid5/1533795689087/1533795686552.ts",
-    }
+	m3 := map[string]interface{}{
+		"duration": 9.88,
+		"url":      "http://pcgtsa42m.bkt.clouddn.com/7/testuid5/testdeviceid5/1533795689087/1533795686552.ts",
+	}
 
-    playlist = append(playlist, m1)
-    playlist = append(playlist, m2)
-    playlist = append(playlist, m3)
+	playlist = append(playlist, m1)
+	playlist = append(playlist, m2)
+	playlist = append(playlist, m3)
 
-    res := Mkm3u8( playlist )
-    fmt.Println( res )
+	res := Mkm3u8(playlist)
+	fmt.Println(res)
 }
