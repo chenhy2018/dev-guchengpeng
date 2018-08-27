5babf82143fef7d65de2085f785d0d6d68a1c040
Gu Edgar <edgar@Gus-MacBook-Pro.local>
2018-08-27 17:29:21 +0800

    coding standard

--- link/vod/src/qiniu.com/main/main.go
+++ link/vod/src/qiniu.com/main/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"os"
+        "fmt"
+        "os"
 
-	"github.com/gin-gonic/gin"
-	"qiniu.com/controllers"
-	"qiniu.com/db"
-	"qiniu.com/models"
-	"qiniu.com/system"
+        "github.com/gin-gonic/gin"
+        "qiniu.com/controllers"
+        "qiniu.com/db"
+        "qiniu.com/models"
+        "qiniu.com/system"
 )
 
 func main() {
 
-	r := gin.Default()
-	Config, err := system.LoadConf("conf.js")
-	if err != nil {
-		fmt.Println("read conf file error, error = ", err)
-		os.Exit(3)
-	}
-	initDb()
+        r := gin.Default()
+        Config, err := system.LoadConf("conf.js")
+        if err != nil {
+                fmt.Println("read conf file error, error = ", err)
+                os.Exit(3)
+        }
+        initDb()
 
-	r.GET("/v1/namespaces/:namespace/uas/:uaid/playback", controllers.GetPlayBackm3u8)
-	r.GET("/v1/namespaces/:namespace/uas/:uaid/segments", controllers.GetSegments)
-	r.GET("/v1/namespaces/:namespace/uas/:uaid/frames", controllers.GetFrames)
+        r.GET("/v1/namespaces/:namespace/uas/:uaid/playback", controllers.GetPlayBackm3u8)
+        r.GET("/v1/namespaces/:namespace/uas/:uaid/segments", controllers.GetSegments)
+        r.GET("/v1/namespaces/:namespace/uas/:uaid/frames", controllers.GetFrames)
         r.POST("/v1/namespaces/:namespace/uas", controllers.RegisterUa)
         r.DELETE("/v1/namespaces/:namespace/uas/:uaid", controllers.DeleteUa)
         r.PUT("/v1/namespaces/:namespace/uas/:uaid", controllers.UpdateUa)
@@ -36,30 +36,30 @@ func main() {
         r.GET("/v1/uids/:uid/namespaces/:namespace", controllers.GetNamespaceInfo)
         r.GET("/v1/uids/:uid/namespaces", controllers.GetNamespaceInfos)
 
-	r.POST("/qiniu/upload/callback", controllers.UploadTs)
-	r.Run(Config.Bind) // listen and serve on 0.0.0.0:8080
+        r.POST("/qiniu/upload/callback", controllers.UploadTs)
+        r.Run(Config.Bind) // listen and serve on 0.0.0.0:8080
 
 }
 
 func initDb() {
-	url := "mongodb://root:public@180.97.147.164:27017,180.97.147.179:27017/admin"
-	dbName := "vod"
-	config := db.MgoConfig{
-		Host:     url,
-		DB:       dbName,
-		Mode:     "",
-		Username: "root",
-		Password: "public",
-		AuthDB:   "admin",
-		Proxies:  nil,
-	}
-	if err := db.InitDb(&config); err != nil {
-		fmt.Println(err)
-		os.Exit(3)
-	}
-	segment := models.SegmentModel{}
-	if err := segment.Init(); err != nil {
-		fmt.Println(err)
-		os.Exit(3)
-	}
+        url := "mongodb://root:public@180.97.147.164:27017,180.97.147.179:27017/admin"
+        dbName := "vod"
+        config := db.MgoConfig{
+                Host:     url,
+                DB:       dbName,
+                Mode:     "",
+                Username: "root",
+                Password: "public",
+                AuthDB:   "admin",
+                Proxies:  nil,
+        }
+        if err := db.InitDb(&config); err != nil {
+                fmt.Println(err)
+                os.Exit(3)
+        }
+        segment := models.SegmentModel{}
+        if err := segment.Init(); err != nil {
+                fmt.Println(err)
+                os.Exit(3)
+        }
 }
