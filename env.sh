d48a00a97c0198266070e0c14557606cc1f7d57f
Gu Edgar <edgar@Gus-MacBook-Pro.local>
2018-09-04 19:10:23 +0800

    add test code

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 export QBOXROOT="${PWD}/third_party"
-export GOPATH=$GOPATH:$QBOXROOT/vendor/:${PWD}/link/vod
 source "$QBOXROOT/base/env.sh"
 source "$QBOXROOT/base/mockacc/env.sh"
 source "$QBOXROOT/apigate/env.sh"
+export GOPATH=$GOPATH:$QBOXROOT/vendor/:${PWD}/link/vod
+export GOPATH=$GOPATH:$HOME/gopath
+export PATH=$HOME/gopath/bin:$PATH
 export GIN_MODE=release
