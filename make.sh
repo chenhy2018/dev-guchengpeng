787a1232e56076434c66135d89b6612290e51dd9
liyq <liyuanquan@qiniu.com>
2018-09-30 11:30:52 +0800

    otimize the build system

--- make.sh
+++ make.sh
@@ -5,9 +5,7 @@ if [ $# != 1 ];then
 fi
 
 source env.sh
-
 export ARCH=$1
-
 export WITH_P2P="OFF"
 export WITH_OPENSSL="OFF"
 export WITH_FFMPEG="OFF"
@@ -15,10 +13,18 @@ export WITH_PJSIP="OFF"
 
 prefix=
 if [ "$1" = "mstar" ];then
-    export CC=arm-linux-gnueabihf-gcc
-    export CXX=arm-linux-gnueabihf-g++
-    export HOST=arm-linux-gnueabihf
-    export LIBPREFIX=arm-unknown-linux-gnueabihf
+   export CC=arm-linux-gnueabihf-gcc
+   export CXX=arm-linux-gnueabihf-g++
+   export HOST=arm-linux-gnueabihf
+   export LIBPREFIX=arm-unknown-linux-gnueabihf
+elif [ "$1" == "hi" ];then
+    export CC=arm-hisiv300-linux-uclibcgnueabi-gcc
+    export CXX=arm-hisiv300-linux-uclibcgnueabi-g++
+    export HOST=arm-hisiv300-linux-uclibcgnueabi
+elif [ "$1" == "80386" ];then
+     export CC=gcc
+     export CXX=g++
+     export CFLAGS=-m32
 elif [ "$1" = "a12" ];then
     export CC=gcc
     export CXX=g++
@@ -32,7 +38,7 @@ elif [ "$1" = "clean" ];then
 fi
 
 if [ -f CMakeCache.txt ];then
-rm CMakeCache.txt
+    rm CMakeCache.txt
 fi
 
 cmake .
@@ -46,7 +52,12 @@ if [ "$WITH_PJSIP" = "ON" ];then
 fi
 
 if [ "$ARCH" = "mstar" ];then
+	cp -rvf link/libtsuploader/ipc-testupload /home/share/ipc-testupload-dbg
 	arm-linux-gnueabihf-strip link/libtsuploader/ipc-testupload
 	cp -rvf link/libtsuploader/ipc-testupload /home/share/
+fi
+
+if [ "$ARCH" = "hi" ] || [ "$ARCH" = "80386" ];then
+    ./make-link-sdk.sh
 fi
 
